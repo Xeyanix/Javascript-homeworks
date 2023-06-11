2977add8b71fd34f5d8caf7a383f9917188ddfe9
Xeyanix <jan.goralewski@o2.pl>
2023-06-12 00:42:32 +0200

    DONE

--- Zaliczenie/BouncingBall/bouncing-ball/src/App.js
+++ Zaliczenie/BouncingBall/bouncing-ball/src/App.js
@@ -10,7 +10,7 @@ const boardData = [
   ['X', '0', '0', '0', '0', '0', '0', '0', 'X', 'X', 'X', 'X'],
   ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
   ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
-  ['X', '0', '0', '0', 'X', '0', '0', '0', '0', '0', 'Y', 'X'],
+  ['X', '0', '0', '0', 'X', '0', '0', '0', '0', 'Y', '0', 'X'],
   ['X', '0', '0', 'X', 'X', 'X', '0', '0', '0', '0', '0', 'X'],
   ['X', '0', '0', '0', 'X', '0', '0', '0', '0', '0', '0', 'X'],
   ['X', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'X'],
@@ -22,46 +22,37 @@ const boardData = [
 const BouncingBallGame = () => {
   const [ballPosition, setBallPosition] = useState({ x: 1, y: 1 });
   const [ballDirection, setBallDirection] = useState({ dx: 1, dy: 1 });
+
+
   useEffect(() => {
     const interval = setInterval(() => {
-      // Update ball position based on direction
       const { x, y } = ballPosition;
       const { dx, dy } = ballDirection;
-
       const newX = x + dx;
       const newY = y + dy;
 
-      if (boardData[newY][newX] === 'X' || boardData[newY][newX] === 'Y') {
-        let newDX = dx;
-        let newDY = dy;
-
-        if (
-          boardData[x][newX] === 'X' ||
-          boardData[newY][y + 1] === 'X' ||
-          boardData[newY][newX] === 'Y'
-        ) {
-          newDX = -dx;
-        }
-        if (
-          boardData[newY][x] === 'X' ||
-          boardData[newY][y] === 'X' ||
-          boardData[x][newY] === 'Y'
-        ) {
-          newDY = -dy;
-        }
-
-        // Set the new direction
+      if (boardData[newY][newX] === 'X') {
+        let newDX = Math.random() > 0.5 ? -dx : dx;
+        let newDY = Math.random() > 0.5 ? -dy : dy;
         setBallDirection({ dx: newDX, dy: newDY });
+
+      } else if (boardData[newY][newX] === 'Y') {
+        let newDX = Math.random() > 0.5 ? -dx : dx;
+        let newDY = Math.random() > 0.5 ? -dy : dy;
+        setBallPosition({ x: newX, y: newY });
+        boardData[newY][newX] = 'X'; // Change 'Y' to 'X'
+        setBallDirection({ dx: newDX, dy: newDY });
+
       } else {
-        // Update ball position
         setBallPosition({ x: newX, y: newY });
       }
-    }, 100);
+    }, 20);
 
     return () => {
       clearInterval(interval);
     };
   }, [ballPosition, ballDirection]);
+
 
   return (
     <div>
@@ -69,6 +60,7 @@ const BouncingBallGame = () => {
         <div key={rowIndex} style={{ display: 'flex' }}>
           {row.map((col, colIndex) => (
             <div
+
               key={colIndex}
               style={{
                 width: 50,
